150c817280a5fc84bf1c248686d06c756bae5801
WAN JIA XING <jiaxing.wan@redone.com.my>
2024-12-12 10:43:28 +0800

    Added Hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +9,7 @@ function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
